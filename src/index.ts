37a0492b26aea2be59318dc0198901cd0f5e82ed
cstef <colin@petit-suisse.fr>
2022-01-12 00:27:41 +0100

    Update WsceConfig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import type { ScraperOptions } from "./scraper";
+import type { ExporterOptions } from "./exporter";
 export { Scraper, ScraperOptions } from "./scraper";
 export { Exporter, ExporterOptions } from "./exporter";
-export interface WsceConfig {}
+export interface WsceConfig {
+    scraper: ScraperOptions;
+    exporter: ExporterOptions;
+}
