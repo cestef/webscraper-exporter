57c0408c78df5767ae3e0285bcc6f8d8f50a60d2
cstef <colin@petit-suisse.fr>
2022-01-12 18:59:52 +0100

    Fix version display

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,16 +6,19 @@ import { hideBin } from "yargs/helpers";
 import Logger from "../Logger.js";
 import * as init from "./commands/init.js";
 import * as start from "./commands/start.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { readFileSync } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 yargs(hideBin(process.argv))
     .scriptName("wsce")
+    .version(JSON.parse(readFileSync(join(__dirname, "../..", "package.json"), "utf8")).version)
     .help()
     .alias("h", "help")
     .command(init as any)
     .command(start as any)
     .demandCommand()
-    .detectLocale(false)
-    .parseAsync()
-    .then((args) => {
-        const logger = new Logger(true, args.v as number);
-        if (!isInstalledGlobally) logger.debug(`Running on local installation`);
-    });
+    .detectLocale(false).argv;
