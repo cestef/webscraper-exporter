f6a596a784995bdd09ddb99824d475669a776c42
cstef <colin@petit-suisse.fr>
2022-02-01 09:47:14 +0100

    Upgrade puppeteer to latest version

--- src/cli/commands/start.ts
+++ src/cli/commands/start.ts
@@ -53,7 +53,7 @@ export const handler = async (args: any) => {
         const imported = await import(args.config.trim());
         config = imported?.default || imported;
     } catch (e) {
-        logger.debug("Couldn't load absolute path, trying relative: ", e);
+        logger.debug("Couldn't load absolute path, trying relative: " + e);
         try {
             const imported = await import(join(process.cwd().trim(), args.config.trim()));
             config = imported?.default || imported;
